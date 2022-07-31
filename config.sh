f0dea2114365b6e15be7377fd38cbb3e887609ed
deyaberger <bergerdeya@gmail.com>
2022-07-31 19:58:30 +0200

    avec p1 done

--- config.sh
+++ config.sh
@@ -2,6 +2,7 @@
 sudo add-apt-repository ppa:gns3/ppa
 sudo apt-get update
 sudo apt install gnome-session-flashback
+# Installing GNS3 and Docker:
 sudo apt install gns3-gui
 sudo apt install docker.io
 echo "You now have to reboot your computer"
--- config2.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-sudo usermod -a -G docker bgp
-docker pull deyaberger/router_dberger
-docker pull alpine
